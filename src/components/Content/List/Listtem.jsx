c0cc2c71c73d9f8151cefd105cda086de8a659c5
humeyrakoseoglu <20180808015@ogr.akdeniz.edu.tr>
2023-05-16 18:14:02 +0300

    To-Do Checkbox İşlemleri

--- src/components/Content/List/Listtem.jsx
+++ src/components/Content/List/Listtem.jsx
@@ -1,9 +1,15 @@
+import {useTodo} from "../../../context/TodoContext"
+
 function ListItem({todo}) {
+	const {todos, setTodos, toggleTodo} =  useTodo();
   
   return (
     <li key={todo.id} className={todo.completed ? "completed" : ""}>
 		<div className="view">
-			<input className="toggle" type="checkbox" checked={todo.completed}/>
+			<input className="toggle" 
+			type="checkbox" 
+			checked={todo.completed} 
+			onChange={()=> toggleTodo(todo.id)}/>
 			<label>{todo.text}</label>
 			<button className="destroy"></button>
 		</div>
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -18,10 +18,19 @@ export const TodoProvider = ({children}) => {
 
     const addTodo = (text) => setTodos((prev) => [...prev, {id: uuidv4(), completed: false, text}]);
 
+    const toggleTodo = (id) => {
+        const cloned_todos = [...todos];
+		const update_item_index = cloned_todos.findIndex(todo => todo.id === id);
+		const item = todos[update_item_index];
+		item.completed = !item.completed;
+		setTodos(cloned_todos);
+    }
+
     const values = {
         todos,
         setTodos,
         addTodo,
+        toggleTodo
     }
 
     return <TodoContext.Provider value = {values}>{children}</TodoContext.Provider>
